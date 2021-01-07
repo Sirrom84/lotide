bb3cbef5fabbe6261ae5837ae5e43ab0c8b330f6
Rob <morrisrjc@gmail.com>
2021-01-07 04:37:56 +0000

    Created function

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const countLetters = function(word) {
+  let obj = {};
+  for (let letter of word) {
+    if (letter in obj) {
+      obj[letter]++;
+    } else {
+      obj[letter] = 1;
+    }
+  
+  }
+  return obj;
+};
+
