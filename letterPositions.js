c6fee9aba3b466596ac188d399f43369bf7ec680
Rob <morrisrjc@gmail.com>
2021-01-07 06:34:56 +0000

    First working crack

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(word) {
+  word = word.toLowerCase();
+  let obj = {};
+  
+  for (let i = 0; i < word.length; i++) {
+    let key = word[i];
+    
+    if (obj[key]) {
+      obj[key].push(i);
+    } else if (!obj[key] && key !== " ") {
+      obj[key] = [i];
+    } else {
+      continue;
+    }
+  }
+  return obj;
+};
+
+console.log(letterPositions("hello"));
