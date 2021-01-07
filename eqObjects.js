bc4318e44786bd29037e2f69ce24c07b162db512
Rob <morrisrjc@gmail.com>
2021-01-07 20:48:03 +0000

    Eual objects ? function

---
+++ eqObjects.js
@@ -0,0 +1,11 @@
+
+const eqObjects = function(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (const key in obj1) {
+    if ((obj1[key]) !== (obj2[key])) {
+      return false;
+    }
+  } return true;
+};
