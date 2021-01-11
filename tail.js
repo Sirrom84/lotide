1eb8cf3dce32f6942debd5c944f21a28dd29d222
Rob <morrisrjc@gmail.com>
2021-01-11 21:42:49 +0000

    refactoring and adding tests

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion failed: ${actual} != ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let output = [];
   for (let i = 1; i <= array.length - 1; i++) {
@@ -14,8 +6,5 @@ const tail = function(array) {
   return output;
 };
 
-console.log(tail([1,2,3,4,5]));
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail
