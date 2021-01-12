218d20a8a6b602d0d8eef62cbdc0832b0e819d4b
Rob <morrisrjc@gmail.com>
2021-01-12 03:16:57 +0000

    had to rework this problem it's fixed now

--- middle.js
+++ middle.js
@@ -1,39 +1,18 @@
-// TEST/ASSERTION FUNCTIONS
-//const assertArraysEqual = function(array1, array2) {
-//   if (array1.length !== array2.length) {
-//     return console.log(`🛑 These arrays are not the same length`);
-//   }
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return console.log(`❌ ${array1} !== ${array2}`);
-//     }
-//   }
-//   return console.log(`✅ Assertion passed ${array1} === ${array2}`)
-// };
-
-// const eqArrays = function(array1, array2) {
-//   if (array1.length !== array2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-
-const middle = function(arrayToMiddle) {
-  let index = [];
-  let emptyArray = [];
-  if (arrayToMiddle.length <= 2) {
-    return emptyArray;
-  } else if (arrayToMiddle.length % 2 !== 0) {
-    index.push(Math.round(arrayToMiddle.length / 2));
-    return index;
-  } else if (arrayToMiddle.length % 2 === 0) {
-    index.push(arrayToMiddle.length / 2,arrayToMiddle.length / 2 + 1);
-    return index;
+const middle = function (array) {
+  let middleIndex = [];
+  if (array.length <= 2) {
+    middleIndex = [];
+   
+  } else if (array.length % 2 !== 0) {
+    middleIndex = [array[Math.round(array.length / 2) - 1]];
+   
+  } else if (array.length % 2 === 0) {
+    middleIndex = [
+      array[Math.round(array.length / 2 - 1)],
+      array[Math.round(array.length / 2)],
+    ];
   }
+  return middleIndex;
 };
+
+module.exports = middle;
